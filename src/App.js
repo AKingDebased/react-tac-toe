92798b143d351a92b8571bcfe218816c5d852d84
gabriel gonzalvez <ggonzalvez@goboomtown.com>
2020-03-15 19:42:17 -0700

    implement basic victory condition

--- src/App.js
+++ src/App.js
@@ -1,17 +1,67 @@
 import React from 'react';
 // TODO: Only import the firebase modules i need
 import firebase from './firebase-config';
+import update from 'immutability-helper';
 
 class App extends React.Component {
 	constructor (props) {
 		super(props);
 		this.state = {
-			currentTurn: 'X'
+			currentTurn: 'X',
+			selectedSquares: []
 		}
 
-		// this.handleSquareClick = this.handleSquareClick.bind(this);
+		this.squareIds = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 		firebase.database();
+	}
+
+	checkGameState (selectedSquares) {
+		const winConditions = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		  ];
+
+		
+		for (let i = 0; i < winConditions.length; i++) {
+			const [a, b, c] = winConditions[i];
+
+			if (!selectedSquares[a] || !selectedSquares[b] || !selectedSquares [c]) continue;
+
+
+			if (selectedSquares[a].symbol && selectedSquares[a].symbol === selectedSquares[b].symbol && selectedSquares[a].symbol === selectedSquares[c].symbol) {
+				return selectedSquares[a];
+			}
+		}
+		
+		return null;
+	}
+
+	handleSquareClick (id) {
+		this.setState((state) => {
+			// TODO: This needs to be respect immutability
+			const selectedSquaresCopy = state.selectedSquares;
+
+			selectedSquaresCopy[id] = { id: id, symbol: state.currentTurn};
+
+			return {
+				...state,
+				selectedSquares: selectedSquaresCopy
+			}
+		}, () => {
+			if (this.checkGameState(this.state.selectedSquares)) {
+				alert('we have a winner!');
+				return;
+			}
+	
+			this.toggleCurrentTurn();
+		});
 	}
 
 	toggleCurrentTurn () {
@@ -22,7 +72,12 @@ class App extends React.Component {
 		return (
 			<div className="App">
 				<GameState currentTurn={this.state.currentTurn}/>
-				<Board currentTurn={this.state.currentTurn} toggleCurrentTurn={this.toggleCurrentTurn.bind(this)}/>
+				<Board 
+					handleSquareClick={this.handleSquareClick.bind(this)} 
+					squareIds={this.squareIds} currentTurn={this.state.currentTurn} 
+					toggleCurrentTurn={this.toggleCurrentTurn.bind(this)}
+					selectedSquares={this.state.selectedSquares}
+				/>
 			</div>
 		);
 	}
@@ -33,21 +88,16 @@ class Board extends React.Component {
 		super(props);
 	}
 
-	createBoard () {
-		// TODO: Probably a better way to do IDs, huh?
-		const boardIds = [1, 2, 3, 4, 5, 6, 7, 8, 9],
-			squares = [];
-
-		for (let i = 0; i < boardIds.length; i++) {
-			squares.push(<Square key={boardIds[i]} currentTurn={this.props.currentTurn} toggleCurrentTurn={this.props.toggleCurrentTurn}/>)
-		}
-
-		return squares;
-	}
-
 	render () {
 		return (
-			<div className="board">{this.createBoard()}</div>
+			<div className="board">{this.props.squareIds.map((squareId) => {
+				return <Square 
+					key={squareId} 
+					squareId={squareId} 
+					handleSquareClick={this.props.handleSquareClick}
+					selectedSquares={this.props.selectedSquares}
+				/>
+			})}</div>
 		);
 	}
 }
@@ -58,25 +108,33 @@ function GameState(props) {
 	);
 }
 
+function Symbol(props) {
+	return (
+		// TODO: These are slightly off center, why?
+		<div className="symbol">{props.symbol}</div>
+	)
+}
+
 class Square extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			symbol: null
-		}
 	}
 
-	handleClick (e) {
-		this.setState((state, props) => {
-			return { symbol: props.currentTurn };
-		});
+	handleClick (squareId) {
+		this.props.handleSquareClick(squareId);
+	}
 
-		this.props.toggleCurrentTurn();
+	renderSymbol () {
+		const currentSquare = this.props.selectedSquares[this.props.squareId];
+
+		return currentSquare ? currentSquare.symbol : null;
 	}
 
 	render () {
 		return (
-			<div onClick={this.handleClick.bind(this)} className="square">{this.state.symbol}</div>
+			<div onClick={this.handleClick.bind(this, this.props.squareId)} className="square">
+				<Symbol symbol={this.renderSymbol()} />	
+			</div>
 		)
 	}
 }
